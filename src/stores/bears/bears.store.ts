fbacfe0840a63a5df2dfcf6f5fb8032fb04b5103
renier1989 <renier1989@gmail.com>
2023-11-07 16:59:46 -0500

    Consumiendo nuestro Store

--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -9,11 +9,13 @@ interface BearState{
   increaseBlackBears : (by:number)=> void,
 }
 
-const useBearStore = create<BearState>()((set) => ({
-  blackBears: 10,
+export const useBearStore = create<BearState>()((set) => ({
+  blackBears: 12,
   polarBears: 10,
   pandaBears: 10,
 
-  increaseBlackBears: (by:number) => set((state) => ({ blackBears: state.blackBears + 1 })),
+  increaseBlackBears: (by:number) => set((state) => ({ blackBears: state.blackBears + by })),
+  increasePolarBears: (by:number) => set((state) => ({ polarBears: state.polarBears + by })),
+  increasePandaBears: (by:number) => set((state) => ({ pandaBears: state.pandaBears + by })),
   
 }))
